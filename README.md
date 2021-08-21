b8560b8db9bdc4477e5dd38ad1cd57aa88b0ef57
Hzdn04 <picman.h@gmail.com>
2021-08-21 17:54:18 +0700

    first commit vue js

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# kreasikita
-website desaign interior yang menjual peralatan-peralatan yang modern dan aesthetic
+# marketplace
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <!-- Header Section Begin -->
+      <HeaderMp />
+    <!-- Header End -->
+
+    <!-- Hero Section Begin -->
+      <HeroMp />
+    <!-- Hero Section End -->
+
+    <!-- Women Banner Section Begin -->
+      <MenuProduk />
+    <!-- Women Banner Section End -->
+
+    <!-- Instagram Section Begin -->
+      <InstagramMenu/>
+    <!-- Instagram Section End -->
+
+    <!-- Partner Logo Section Begin -->
+      <PartnerLogo />
+    <!-- Partner Logo Section End -->
+
+    <!-- Footer Section Begin -->
+      <Footer />
+    <!-- Footer Section End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderMp from '@/components/HeaderMp.vue'
+import HeroMp from '@/components/HeroMp.vue'
+import MenuProduk from '@/components/MenuProduk.vue'
+import InstagramMenu from '@/components/InstagramMenu.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderMp,
+    HeroMp,
+    MenuProduk,
+    InstagramMenu,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
